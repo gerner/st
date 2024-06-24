f7968221ad3c79ae6898637c5499c306f62a8197
Nick Gerner <nick.gerner@gmail.com>
2024-06-24 14:56:35 -0700

    tweaking fonts

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Liberation Mono:size=12:antialias=false:autohint=false";
+static char font[] = "Liberation Mono-12";//:pixelsize=14:antialias=false:autohint=false";
 static int borderpx = 2;
 static int histsize = 2000;
 static char shell[] = "/bin/sh";
--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Liberation Mono:pixelsize=12:antialias=true:autohint=false";
+static char font[] = "DejaVu Sans Mono:pixelsize=12:antialias=false:autohint=false";//"Terminus:antialias=false:autohint:false:size=14";
 static int borderpx = 2;
 static int histsize = 2000;
 static char shell[] = "/bin/sh";
